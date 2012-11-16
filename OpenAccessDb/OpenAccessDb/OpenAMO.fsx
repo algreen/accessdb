af36a3891f1f410d2483619524f47e459b507d6d
Alan Green <agreen@lizearle.com>
2012-11-16 16:49:15 +0000

    removed code that isnt working

--- OpenAccessDb/OpenAccessDb/OpenAMO.fsx
+++ OpenAccessDb/OpenAccessDb/OpenAMO.fsx
@@ -6,33 +6,33 @@ open System.IO
 open System
 
 //let OpenAccessFile = 
-    let openAccess = File.ReadAllBytes("D:\\Amo.accdb")
+let openAccess = File.ReadAllBytes("D:\\Amo.accdb")
 
-    let openAccessDb = File.WriteAllLines("D:\\Amo.accdb")
-
-
-open System.Windows.Forms
-open System.Data.OleDb
-open System.Data
-
-//Create winform//
-let frmMain = new Form()
-
-//Connect to Access Db//
-let ADOCon = new OleDbConnection(@"Provider=Microsoft.Jet.OLEDB.4.0;
-  Data Source=C:\Users\...\Desktop\FSharpDb.mdb")
-
-let DAdapter = new OleDbDataAdapter("Select * from Names_Table", ADOCon)
-
-let DTable = new DataTable()
-DAdapter.Fill(DTable)|>ignore
-let view = new DataGridView()
-do view.DataSource <- DTable
-
-let ConnectionString = 
-    ADOCon.Open()
-
-frmMain.Controls.Add(view)
-
-//Run main form on start up
-Application.Run(frmMain)
+//let openAccessDb = File.WriteAllLines("D:\\Amo.accdb")
+//
+//
+//open System.Windows.Forms
+//open System.Data.OleDb
+//open System.Data
+//
+////Create winform//
+//let frmMain = new Form()
+//
+////Connect to Access Db//
+//let ADOCon = new OleDbConnection(@"Provider=Microsoft.Jet.OLEDB.4.0;
+//  Data Source=C:\Users\...\Desktop\FSharpDb.mdb")
+//
+//let DAdapter = new OleDbDataAdapter("Select * from Names_Table", ADOCon)
+//
+//let DTable = new DataTable()
+//DAdapter.Fill(DTable)|>ignore
+//let view = new DataGridView()
+//do view.DataSource <- DTable
+//
+//let ConnectionString = 
+//    ADOCon.Open()
+//
+//frmMain.Controls.Add(view)
+//
+////Run main form on start up
+//Application.Run(frmMain)
