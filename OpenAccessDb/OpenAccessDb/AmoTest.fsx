849dc1374fe589cebcbd2ef233d5d8879d237840
Alan Green <agreen@lizearle.com>
2012-11-19 16:48:51 +0000

    For Matt to look at

--- OpenAccessDb/OpenAccessDb/AmoTest.fsx
+++ OpenAccessDb/OpenAccessDb/AmoTest.fsx
@@ -1,11 +1,16 @@
 ﻿
 
-open System
-open System.Data
-open System.Data.Linq
+#I "C:\Program Files (x86)\Reference Assemblies\Microsoft\FSharp\3.0\Runtime\v4.0\Type Providers\FSharp.Data.TypeProviders.dll"
+#r "FSharp.Data.TypeProviders"
 open Microsoft.FSharp.Data.TypeProviders
 
-type amoSchema = SqlDataConnection<"Data Source=DEV02A\DEV2005; Initial Catalog=amo;Integrated Security=SSPI;">
+// to get the interactive window to run with the DLL i want
+#I "C:\Program Files (x86)\Reference Assemblies\Microsoft\Framework\.NETFramework\v4.5\System.Data.Linq.dll"
+#r "System.Data.Linq" ;;
+open System.Data.Linq;;
+
+
+type amoSchema = SqlDataConnection<"Data Source=DEV02A\DEV2005;Initial Catalog=amo;Integrated Security=SSPI;">
 let db = amoSchema.GetDataContext()
 
 db.DataContext.Log <- System.Console.Out
@@ -19,39 +24,4 @@ let query1 =
             select row
     }
 query1 |> Seq.iter (fun row -> printfn "%s %d" row.LastName row.CustomerID)
-
-
-let query2 = 
-    query {
-        for row in db.TblCustomers do
-        join row2 in db.TblOrders on (row.CustomerID = row2.CustomerID)
-        select (row, row2)
-    }
-
-// find specific data
-let findCustomer customerId =
-    query {
-        for row in db.TblCustomers do
-        where (row.CustomerID = customerId)
-        select row
-        }
-
-findCustomer 100 |> Seq.iter (fun row -> printfn "Found row: %d %s" row.CustomerID row.LastName)
-
-// calling Stored Procs, have to pass in StoredProcs = true
-
-type storedProcSchema = SqlDataConnection<"Data Source=DEV02a\DEV2005; Initial Catalog=amo;Integrated Security=SSPI", StoredProcedures=true>
-
-let testdb = storedProcSchema.GetDataContext()
-
-// nullable parameters to be passed across as nullable
-let nullable value = new System.Nullable<_>(value)
-
-let calcTotalSpendStoredProc customerId =
-    let spendResults = testdb.Pr_AMO_CalcTotalSpend(nullable customerId)
-  //  for result in spendResults do
-        printfn "%d" (spendResults.ToString()) // should be something like (results.Data.GetValueorDefault()
-  
-        spendResults.Return
-
 
