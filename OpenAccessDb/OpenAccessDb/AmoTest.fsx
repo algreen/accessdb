6e8806385f4eb4670092b12f9e3a0bda955457c2
Alan Green <algreengo@googlemail.com>
2012-11-19 22:13:41 +0000

    starting to consume a web service

--- OpenAccessDb/OpenAccessDb/AmoTest.fsx
+++ OpenAccessDb/OpenAccessDb/AmoTest.fsx
@@ -1,4 +1,4 @@
-﻿
+﻿// Crib sheet http://msdn.microsoft.com/en-us/library/hh361033.aspx
 
 #I "C:\Program Files (x86)\Reference Assemblies\Microsoft\FSharp\3.0\Runtime\v4.0\Type Providers\FSharp.Data.TypeProviders.dll"
 #r "FSharp.Data.TypeProviders"
@@ -24,4 +24,70 @@ let query1 =
             select row
     }
 query1 |> Seq.iter (fun row -> printfn "%s %d" row.LastName row.CustomerID)
+
+let query2 = 
+    query {
+        for row in db.TblCustomers do
+        join row2 in db.TblOrders on (row.CustomerID = row2.CustomerID)
+        select (row, row2)
+    }
+
+// find specific data
+let findCustomer customerId =
+    query {
+        for row in db.TblCustomers do
+        where (row.CustomerID = customerId)
+        select row
+        }
+
+findCustomer 100 |> Seq.iter (fun row -> printfn "Found row: %d %s" row.CustomerID row.LastName)
+
+// calling Stored Procs, have to pass in StoredProcs = true
+
+type storedProcSchema = SqlDataConnection<"Data Source=DEV02a\DEV2005; Initial Catalog=amo;Integrated Security=SSPI", StoredProcedures=true>
+
+let testdb = storedProcSchema.GetDataContext()
+
+// nullable parameters to be passed across as nullable
+let nullable value = new System.Nullable<_>(value)
+//
+//let calcTotalSpendStoredProc customerId =
+//    let spendResults = testdb.Pr_AMO_CalcTotalSpend(nullable customerId)
+//  //  for result in spendResults do
+//        printfn "%d" (spendResults.ToString()) // should be something like (results.Data.GetValueorDefault()
+//  
+//        spendResults.Return
+
+// Inserting or updating the database
+
+//let newRecord = new dbSchema.ServiceTypes.TblCustomers(FirstName = "Ted",
+//                                                        LastName = "Baker")
+//
+//let newValues = 
+//    [
+//        for i in [1 .. 10] ->
+//            new dbSchema.ServiceTypes.Table3(Id = 700 + i,
+//                                            Name = "Testing" + i.ToString(),
+//                                            Data = i) ]
+
+//
+db.Table1.InsertOnSubmit(newRecord)
+db.Table3.InsertAllOnSubmit(newValues)
+try
+    db.DataContext.SubmitChanges()
+    printfn "Successfully inserted new rows"
+with
+    |   exn -> printfn "Exception:\n%s" exn.Message
+
+
+
+
+// Customer Web Service
+
+type CustomerWebService = WsdlService<"http://?wsdl">
+
+try
+
+with
+    |
 
---
+++ OpenAccessDb/OpenAccessDb/ConsumeWebService.fsx
@@ -0,0 +1,28 @@
+﻿
+
+
+#I "C:\Program Files\Reference Assemblies\Microsoft\Framework\.NETFramework\v4.5\System.ServiceModel.dll"
+#r "System.ServiceModel"
+open System.ServiceModel
+open System
+open Microsoft.FSharp.Linq
+#I "C:\Program Files\Reference Assemblies\Microsoft\FSharp\3.0\Runtime\v4.0\Type Providers\FSharp.Data.TypeProviders.dll"
+#r "FSharp.Data.TypeProviders"
+open Microsoft.FSharp.Data.TypeProviders
+
+//type twitter = "http://twitter.com/statuses/user_timeline.xml?id=joelcomm"
+
+type TerraService = WsdlService<"http://msrmaps.com/TerraService2.asmx?WSDL">
+try
+    let terraClient = TerraService.GetTerraServiceSoap ()
+    let myPlace = new TerraService.ServiceTypes.msrmaps.com.Place(City = "Redmond", State = "Washington", Country = "United States")
+    let myLocation = terraClient.ConvertPlaceToLonLatPt(myPlace)
+    printfn "Redmond Latitude: %f Longitude: %f" (myLocation.Lat) (myLocation.Lon)
+with
+    | :? ServerTooBusyException as exn ->
+        let innerMessage =
+            match (exn.InnerException) with
+            | null -> ""
+            | innerExn -> innerExn.Message
+        printfn "An exception occurred:\n %s\n %s" exn.Message innerMessage
+    | exn -> printfn "An exception occurred: %s" exn.Message
