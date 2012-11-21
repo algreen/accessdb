da624a491189d96782b5ee4aef708e4cd653fc4d
Alan Green <algreengo@googlemail.com>
2012-11-21 22:23:10 +0000

    Not a lot done

--- OpenAccessDb/OpenAccessDb/TestRssFeeds.fsx
+++ OpenAccessDb/OpenAccessDb/TestRssFeeds.fsx
@@ -61,6 +61,57 @@ let fetchUrlTest url =
         sites |> List.map fetchUrlTest
 
 
+
+let slowConsoleWrite msg = 
+    msg |> String.iter (fun ch->
+        System.Threading.Thread.Sleep(1)
+        System.Console.Write ch
+        )
+
+// test in isolation
+slowConsoleWrite "abc"
+
+type SerializedLogger() = 
+
+    // create the mailbox processor
+    let agent = MailboxProcessor.Start(fun inbox -> 
+
+        // the message processing function
+        let rec messageLoop () = async{
+
+            // read a message
+            let! msg = inbox.Receive()
+
+            // write it to the log
+            slowConsoleWrite msg
+
+            // loop to top
+            return! messageLoop ()
+            }
+
+        // start the loop
+        messageLoop ()
+        )
+
+    // public interface
+    member this.Log msg = agent.Post msg
+
+// test in isolation
+let serializedLogger = SerializedLogger()
+serializedLogger.Log "hello"
+
+
+let makeTask logger taskId = async {
+    let name = sprintf "Task%i" taskId
+    for i in [1..3] do 
+        let msg = sprintf "-%s:Loop%i-" name i
+        logger msg 
+    }
+
+// test in isolation
+let task = makeTask slowConsoleWrite 1
+Async.RunSynchronously task
+
 let serializedExample = 
     let logger = new SerializedLogger()
     [1..5]
